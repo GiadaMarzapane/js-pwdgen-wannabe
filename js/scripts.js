49c0e7f65412e1b011676106e85ab799a1ca4a87
GiadaMarzapane <giada.ortesta@gmail.com>
2022-12-16 15:17:23 +0100

    Add terza const

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,5 @@ const myname = prompt ('Qual è il tuo nome?');
 console.log ("Il nome dell'utente è: ", myname);
 const mysurname = prompt('Qual è il tuo cognome?');
 console.log("Il cognome dell'utente è: ", mysurname);
+const favcolor = prompt('Qual è il tuo colore preferito?');
+console.log("Il colore preferito dell'utente è: ", favcolor);
