43ecb1306fdf0408e9d6099273879d58adb7487e
GiadaMarzapane <giada.ortesta@gmail.com>
2022-12-16 15:16:06 +0100

    Add seconda const

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,4 @@
-const name = prompt ('Come ti chiami?');
-console.log ("L'utente si chiama: ", name);
+const myname = prompt ('Qual è il tuo nome?');
+console.log ("Il nome dell'utente è: ", myname);
+const mysurname = prompt('Qual è il tuo cognome?');
+console.log("Il cognome dell'utente è: ", mysurname);
