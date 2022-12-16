c9dce5820b88ba18d36cae414b2162d61e8cb95c
GiadaMarzapane <giada.ortesta@gmail.com>
2022-12-16 15:23:32 +0100

    Fine esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,8 @@ const mysurname = prompt('Qual è il tuo cognome?');
 console.log("Il cognome dell'utente è: ", mysurname);
 const favcolor = prompt('Qual è il tuo colore preferito?');
 console.log("Il colore preferito dell'utente è: ", favcolor);
+
+const pwdgen = myname + mysurname + favcolor + '21';
+console.log('La password sarà: ', pwdgen);
+
+document.writeln(pwdgen);
